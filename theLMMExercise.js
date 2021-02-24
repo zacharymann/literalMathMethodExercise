81746ba6d7301066e49bee1b3104be375ca4d504
zachmann00 <zach_mann@hotmail.com>
2021-02-24 15:18:59 -0600

    Assignment complete

---
+++ theLMMExercise.js
@@ -0,0 +1,46 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf, and I like warm hugs!"
+// 1b
+console.log(warmHugs.toUpperCase());
+// 1c
+console.log(warmHugs.replace("like", "love"));
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+// 2b
+console.log(beenImpaled.slice(18));
+
+// 3a
+let dotDotDot = "...";
+// 3b
+let skullBones = `I dont have a skull${dotDotDot}or bones`
+console.log(skullBones);
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random()
+randomNumber *=3
+randomNumber = Math.floor(randomNumber)
+randomNumber ++;
+console.log(randomNumber);
+
+// BONUS
+// 6
+let letItGo = ` Let It Go!`;
+console.log(letItGo.variableTrim.repeat(3));
+
+// 7a
+let reindeer = "Reindeers are better than people.";
+// 7b
+console.log(reindeer.replaceAll(" " , "_"));
+
+// 8
+console.log(Math.sqrt(2));
+
+//9
+console.log(Math.random() * (7 - 23) + 7);
+
+
+
